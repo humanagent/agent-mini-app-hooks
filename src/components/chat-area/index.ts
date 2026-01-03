bb6179da12b34b93276bbacbfeda8e893230828a
Fabrizio Guespe <fguespe@gmail.com>
2026-01-03 03:21:42 -0300

    Update

--- src/components/chat-area/index.ts
+++
@@ -1,6 +0,0 @@
-export { ChatArea } from "./chat-area";
-export { ChatHeader } from "./chat-header";
-export { Greeting } from "./greeting";
-export { SuggestedActions } from "./suggested-actions";
-export { Suggestion, Suggestions } from "./suggestion";
-export type { SuggestionProps, SuggestionsProps } from "./suggestion";
--- src/components/sidebar/index.ts
+++
@@ -1,3 +0,0 @@
-export { Sidebar } from "./sidebar";
-export { SidebarUserNav } from "./sidebar-user-nav";
-export { SidebarToggle } from "./sidebar-toggle";
